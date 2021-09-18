29f25bb7c4fe9073542541772f1a48b7bc775e51
Norbert Manthey <nmanthey@conp-solutions.com>
2021-09-18 13:56:29 +0200

    maxsat: add atleast2
    Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

--- AtLeastTwoSolutions.cc
+++ AtLeastTwoSolutions.cc
@@ -47,19 +47,73 @@ void print_formula(Formula &f, std::string s)
     }
 }
 
+void print_maxsat_formula(Formula &hardclauses, std::vector <Lit> penalty_literals, std::string s, bool pre2021format = true)
+{
+    std::cout << "c AtLeastTwoSolutions, Norbert Manthey, 2021" << std::endl;
+    if (!s.empty()) std::cout << "c " << s << std::endl;
+    std::cout << "c print in pre2021 MaxSat format: " << pre2021format << std::endl;
+    std::cout << "c " << std::endl;
+    if(pre2021format)
+    {
+        size_t top = penalty_literals.size() + 1;
+        std::cout << "p wcnf " << hardclauses.nVars() << " " << hardclauses.clauses.size() + penalty_literals.size() << " " << top << std::endl;
+        /* print the soft unit clauses */
+        for (const auto &unit : penalty_literals) {
+            std::stringstream s;
+            s << 1 << " " << unit;
+            printf("%s 0\n", s.str().c_str());
+        }
+        /* print the hard clauses */
+        for (const auto &c : hardclauses.clauses) {
+            std::stringstream s;
+            s << top << " " << c;
+            printf("%s 0\n", s.str().c_str());
+        }
+    } else {
+        /* print the soft unit clauses */
+        for (const auto &unit : penalty_literals) {
+            std::stringstream s;
+            s << 1 << " " << unit;
+            printf("%s 0\n", s.str().c_str());
+        }
+        /* print the hard clauses */
+        for (const auto &c : hardclauses.clauses) {
+            std::stringstream s;
+            s << "h " << c;
+            printf("%s 0\n", s.str().c_str());
+        }
+    }
+
+}
+
 int main(int argc, char **argv)
 {
     int opt;
     int tseitin = 0;
+    int maxsat = 0;
     std::cerr << "c AtLeastTwoSolutions generates a CNF formula " << std::endl
-              << "c which is if the given  input formula has at least 2 models" << std::endl;
+              << "c which is satisfiable if the given input formula has at least 2 models" << std::endl
+              << "c" << std::endl
+              << "c OPTIONS" << std::endl
+              << "c -t x ... only force differences among the variables 1 to x" << std::endl
+              << "c -W   ... encode a MaxSat formula that tries to get two solutions with largest hamming distance" << std::endl
+              << "c -w   ... same as -w, but use the pre 2020 MaxSat format" << std::endl
+              << std::endl;
 
     // Retrieve the options:
-    while ((opt = getopt(argc, argv, "t:")) != -1) { // for each option...
+    while ((opt = getopt(argc, argv, "t:wW")) != -1) { // for each option...
         switch (opt) {
         case 't':
             tseitin = atoi(optarg);
             std::cerr << "c set tseitin variable to " << tseitin << std::endl;
+            break;
+        case 'w':
+            maxsat = 1;
+            std::cerr << "c enable maxsat pre-2021 mode" << std::endl;
+            break;
+        case 'W':
+            maxsat = 2;
+            std::cerr << "c enable maxsat post-2020 mode" << std::endl;
             break;
         case '?': // unknown option...
             std::cerr << "c unknown option: '" << char(optopt) << "'!" << std::endl;
@@ -128,13 +182,22 @@ int main(int argc, char **argv)
         one_unequal_clause.push_back(~next_lit);
     }
 
-    /* one of the common literal pair should have unequal truth values has to be */
+    /* enforce that at least one assignment has to be different */
     result.addClause_(one_unequal_clause);
 
     std::stringstream s;
-    s << "encode formula to check whether there are more than 1 solution for " << fn1;
-    if (tseitin != 0) s << " with tseitin base variable " << tseitin;
-    print_formula(result, s.str());
+    if (maxsat == 0) {
+        s << "encode formula to check whether there are more than 1 solution for " << fn1;
+        if (tseitin != 0) s << " with tseitin base variable " << tseitin;
+        /* one of the common literal pair should have unequal truth values has to be */
+        print_formula(result, s.str());
+    } else {
+        s << "encode formula to find two solutions with the highest hamming distance for a given formula, at least 1, for " << fn1;
+        if (tseitin != 0) s << " with tseitin base variable " << tseitin;
+        /* there is a cost setting variables to equal truth values, hence, pay cost for each unit */
+        print_maxsat_formula(result, one_unequal_clause, s.str(), maxsat == 1);
+    }
+
 
     return 0;
 }
